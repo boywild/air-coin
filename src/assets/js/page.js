adf203afc40bf333ebd1636e2825b14406b1e32a
chentian <chentian@kayak.com>
2021-05-20 14:23:16 +0800

    修改

--- src/assets/js/page.js
+++ src/assets/js/page.js
@@ -1,14 +1,20 @@
 window.onload = function () {
+  // 初始化时间线
   const gsapTimeline = gsap.timeline({ paused: !0 });
+  // 初始化元素获取
   const intro = document.querySelector(".home_intro");
   const videoBg = intro.querySelector(".video-background");
   const introVideo = intro.querySelector(".intro");
   const introLoop = intro.querySelector(".intro-loop");
 
+  // 文字分隔特效
   const BigTitle = Array.from(intro.querySelectorAll(".title span"));
   const titleWord1 = new SplitText(BigTitle[0], { type: "words" });
   const titleWord2 = new SplitText(BigTitle[1], { type: "words" });
-  console.log(BigTitle);
+  const subTitle = new SplitText(document.querySelectorAll(".baseline"), {
+    type: "words,chars",
+  });
+  // 视频播放完成
   introVideo.addEventListener("ended", function (t) {
     introLoop.style.opacity = 1;
     introLoop.style.zIndex = 2;
@@ -16,10 +22,11 @@ window.onload = function () {
   });
 
   gsapTimeline.set(videoBg, { scale: 0.5, autoAlpha: 0 });
-  // gsapTimeline.set(".baseline", { autoAlpha: 0 });
   gsapTimeline.add(function () {
     introVideo.play();
   }, 0.3);
+
+  // 视频背景动画
   gsapTimeline
     .to(
       videoBg,
@@ -36,6 +43,7 @@ window.onload = function () {
     .to(videoBg, 0.2, { autoAlpha: 1 }, 0.3)
     .to(".header", { autoAlpha: 1 }, "+=2");
 
+  // 标题动画ƒ
   titleWord1.words.forEach(function (t, i) {
     TweenLite.set(t, {
       scale: 0.7,
@@ -50,6 +58,7 @@ window.onload = function () {
       0.65 + 0.15 * i
     );
   });
+
   titleWord2.words.forEach(function (t, i) {
     TweenLite.set(t, {
       scale: 0.7,
@@ -65,9 +74,8 @@ window.onload = function () {
     );
   });
 
-  new SplitText(document.querySelectorAll(".baseline"), {
-    type: "words,chars",
-  }).words.forEach(function (t, i) {
+  // 文字描述动画
+  subTitle.words.forEach(function (t, i) {
     console.log(t);
     TweenLite.set(t, { autoAlpha: 0 });
     gsapTimeline.to(t, 0, { autoAlpha: 1 }, 2.98 + 0.7 + 0.09 * i);
