1c938c4e77a04ea66c4f480cef06e0ae00b20bfe
chentian <chentian@kayak.com>
2021-05-20 14:16:41 +0800

    修改

--- src/assets/js/page.js
+++ src/assets/js/page.js
@@ -4,6 +4,11 @@ window.onload = function () {
   const videoBg = intro.querySelector(".video-background");
   const introVideo = intro.querySelector(".intro");
   const introLoop = intro.querySelector(".intro-loop");
+
+  const BigTitle = Array.from(intro.querySelectorAll(".title span"));
+  const titleWord1 = new SplitText(BigTitle[0], { type: "words" });
+  const titleWord2 = new SplitText(BigTitle[1], { type: "words" });
+  console.log(BigTitle);
   introVideo.addEventListener("ended", function (t) {
     introLoop.style.opacity = 1;
     introLoop.style.zIndex = 2;
@@ -11,6 +16,7 @@ window.onload = function () {
   });
 
   gsapTimeline.set(videoBg, { scale: 0.5, autoAlpha: 0 });
+  // gsapTimeline.set(".baseline", { autoAlpha: 0 });
   gsapTimeline.add(function () {
     introVideo.play();
   }, 0.3);
@@ -29,6 +35,46 @@ window.onload = function () {
     )
     .to(videoBg, 0.2, { autoAlpha: 1 }, 0.3)
     .to(".header", { autoAlpha: 1 }, "+=2");
+
+  titleWord1.words.forEach(function (t, i) {
+    TweenLite.set(t, {
+      scale: 0.7,
+      transformOrigin: "50% 80%",
+      autoAlpha: 0,
+    });
+    gsapTimeline.to(t, 0, { autoAlpha: 1 }, 0.55 + 0.15 * i);
+    gsapTimeline.to(
+      t,
+      0.15,
+      { scale: 1, ease: Expo.easeInOut },
+      0.65 + 0.15 * i
+    );
+  });
+  titleWord2.words.forEach(function (t, i) {
+    TweenLite.set(t, {
+      scale: 0.7,
+      transformOrigin: "50% 80%",
+      autoAlpha: 0,
+    });
+    gsapTimeline.to(t, 0, { autoAlpha: 1 }, 2.98 + 0.15 * i);
+    gsapTimeline.to(
+      t,
+      0.15,
+      { scale: 1, ease: Expo.easeInOut },
+      3.08 + 0.15 * i
+    );
+  });
+
+  new SplitText(document.querySelectorAll(".baseline"), {
+    type: "words,chars",
+  }).words.forEach(function (t, i) {
+    console.log(t);
+    TweenLite.set(t, { autoAlpha: 0 });
+    gsapTimeline.to(t, 0, { autoAlpha: 1 }, 2.98 + 0.7 + 0.09 * i);
+  });
+  // gsapTimeline.from(".home_titles .title span", { duration: 3, text: "" });
+  // gsapTimeline.from(".baseline", { duration: 3, text: "" });
+  // .to(".baseline", 0, { autoAlpha: 1 });
 
   gsapTimeline.play();
 };
